491eb32d2f5ca98cf62935c3f35062939e17e062
Jayasri <srij83364@gmail.com>
2026-07-16 11:08:53 +0530

    Initial commit: Django REST API + React job portal

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import RegisterPage from './RegisterPage'
+import LoginPage from './LoginPage'
+import JobsListPage from './JobsListPage'
+import ApplyjobPage from './ApplyjobPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+ 
+  return (
+    <BrowserRouter>
+	    <Routes>
+			<Route path='/register' element={<RegisterPage/>} />
+			<Route path='/login' element={<LoginPage/>} />
+			<Route path='/jobs' element={<JobsListPage/>} />
+			<Route path='/apply/:jobId' element={<ApplyjobPage/>} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
