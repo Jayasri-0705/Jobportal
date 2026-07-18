1bfc6e273b23924f6c2a88e98d734910ac28b269
Jayasri <srij83364@gmail.com>
2026-07-18 15:45:11 +0530

    Add requirements.txt and update settings for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,15 +80,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': "jobportal_db",
-        'USER': 'postgres',
-        'PASSWORD': 'Postgre@123',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:Postgre%40123@localhost:5432/jobportal_db',
+        conn_max_age=600
+    )
 }
 
 
