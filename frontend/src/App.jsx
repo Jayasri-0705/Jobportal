6ed2aad914403177b385dab4c8e47009a7b5cc6a
Jayasri <srij83364@gmail.com>
2026-07-20 07:13:22 +0530

    Show register page as homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
 	  <Routes>
-                 <Route path='/' element={<LoginPage/>} />
+                 <Route path='/' element={<RegisterPage/>} />
                  <Route path='/register' element={<RegisterPage/>} />
                  <Route path='/login' element={<LoginPage/>} />
                  <Route path='/jobs' element={<JobsListPage/>} />
